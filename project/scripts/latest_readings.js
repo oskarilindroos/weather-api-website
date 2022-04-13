2da07c6647de8be7c24a6521179a056ad14b0892
Oskari Lindroos <oskari.a.lindroos@tuni.fi>
2022-04-13 15:47:11 +0300

    add fetching and displaying of latest weather data

---
+++ project/scripts/latest_readings.js
@@ -0,0 +1,42 @@
+const fetchWeatherData = async() => {
+    try {
+        const response = await fetch("http://webapi19sa-1.course.tamk.cloud/v1/weather/limit/50");
+        const jsonData = await response.json();
+        console.log(jsonData);
+        populateTables(jsonData);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const populateTables = (weatherData) => {
+    const tableBody = document.getElementById("tableBody");
+
+    weatherData.map(item => {
+        const row = document.createElement("tr");
+
+        const signalColumn = document.createElement("td");
+        const valueColumn = document.createElement("td");
+        const dateColumn = document.createElement("td");
+        const timeColumn = document.createElement("td");
+
+        for (let key in item.data) {
+            signalColumn.innerHTML = key;
+            valueColumn.innerHTML = item.data[key].toFixed(2); 
+        }
+
+        let date = new Date(item.date_time);
+        dateColumn.innerHTML = date.toLocaleDateString();
+        timeColumn.innerHTML = date.toLocaleTimeString();
+
+        row.appendChild(signalColumn);
+        row.appendChild(valueColumn);
+        row.appendChild(dateColumn);
+        row.appendChild(timeColumn);
+
+        tableBody.appendChild(row);
+    });
+}
+
+fetchWeatherData();
