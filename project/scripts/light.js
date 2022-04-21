ae6b78de3202280524e646d19c1965b0680f0e6c
Oskari Lindroos <oskari.a.lindroos@tuni.fi>
2022-04-21 16:39:13 +0300

    add light data

---
+++ project/scripts/light.js
@@ -0,0 +1,94 @@
+const fetchWeatherData = async (timeSpan) => {
+    try {
+        const response = await fetch(`http://webapi19sa-1.course.tamk.cloud/v1/weather/light/${timeSpan}`);
+        const jsonData = await response.json();
+        loadingAnim.style.visibility = "hidden";
+        populateTables(jsonData);
+        drawChart(jsonData);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const populateTables = (weatherData) => {
+    const tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = "";
+    weatherData.map(item => {
+        const row = document.createElement("tr");
+
+        const dateColumn = document.createElement("td");
+        const timeColumn = document.createElement("td");
+        const valueColumn = document.createElement("td");
+
+        valueColumn.innerHTML = parseFloat(item.light).toFixed(2);
+
+        let date = new Date(item.date_time);
+        dateColumn.innerHTML = date.toLocaleDateString("en-us");
+        timeColumn.innerHTML = date.toLocaleTimeString("en-US");
+
+        row.appendChild(dateColumn);
+        row.appendChild(timeColumn);
+        row.appendChild(valueColumn);
+
+        tableBody.appendChild(row);
+    });
+}
+
+const drawChart = (data) => {
+
+    let dates = [];
+    let values = [];
+    for (var i = 0; i < data.length; i++) {
+        dates[i] = new Date(data[i].date_time).toLocaleString("en-us");
+        values[i] = data[i].light;
+    }
+    const chart = Chart.getChart("myChart");
+
+    if (chart != undefined) {
+        chart.destroy();
+    }
+
+    new Chart("myChart", {
+        type: "line",
+        data: {
+            datasets: [{
+                backgroundColor: "red",
+                borderColor: 'red',
+                data: values,
+                label: "Light"
+            }],
+            labels: dates
+        },
+        options: {
+            scales: {
+                x: {
+                    ticks: {
+                        maxTicksLimit: 3,
+                        maxRotation: 0
+                    }
+                }
+            },
+
+            plugins: {
+                legend: { display: true },
+                title: {
+                    display: false
+                }
+
+            }
+        }
+    });
+
+}
+
+const loadingAnim = document.getElementById("loading");
+
+const selectElement = document.getElementById("timespanSelect");
+selectElement.addEventListener("change", () => {
+    fetchWeatherData(selectElement.value);
+    loadingAnim.style.visibility = "visible";
+});
+
+
+fetchWeatherData(selectElement.value);
